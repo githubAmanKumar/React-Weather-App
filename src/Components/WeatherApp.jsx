d03e089ed61ddc272f09be7eba264a02f4d10e3a
Aman Kumar <amankumarsre5378@gmail.com>
2024-08-08 20:35:54 +0530

    improving icon for day/night and some little changes

--- src/Components/WeatherApp.jsx
+++ src/Components/WeatherApp.jsx
@@ -1,25 +1,35 @@
 import React, { useState } from 'react'
-import cloud_icon from '../assets/cloud.png'
-import warm_icon from '../assets/warm.png'
-import clear_icon from '../assets/clear.png'
-import extremeWarm_icon from '../assets/extremeWarm.png'
-import drizzle_icon from '../assets/drizzle.png'
-import rain_icon from '../assets/rain.png'
-import snow_icon from '../assets/snow.png'
+
+import icon_01d from '../assets/icons/01d.png'
+import icon_01n from '../assets/icons/01n.png'
+import icon_02d from '../assets/icons/02d.png'
+import icon_02n from '../assets/icons/02n.png'
+import icon_03d from '../assets/icons/03d.png'
+import icon_03n from '../assets/icons/03n.png'
+import icon_04d from '../assets/icons/04d.png'
+import icon_04n from '../assets/icons/04n.png'
+import icon_09d from '../assets/icons/09d.png'
+import icon_09n from '../assets/icons/09n.png'
+import icon_10d from '../assets/icons/10d.png'
+import icon_10n from '../assets/icons/10n.png'
+import icon_11d from '../assets/icons/11d.png'
+import icon_11n from '../assets/icons/11n.png'
+import icon_13d from '../assets/icons/13d.png'
+import icon_13n from '../assets/icons/13n.png'
+import icon_50d from '../assets/icons/50d.png'
+import icon_50n from '../assets/icons/50n.png'
+
 
 import search_icon from '../assets/search.png'
 import humidity_icon from '../assets/humidity.png'
 import wind_icon from '../assets/wind.png'
+
 function WeatherApp() {
 
     const [city, setCity] = useState(null)
     const [search, setSearch] = useState("")
-    const [tempImage, setTempImage] = useState(cloud_icon)
+    const [tempImage, setTempImage] = useState()
     const [tempText, setTempText] = useState("")
-
-
-
-
 
     const fetchapi = async () => {
         const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${import.meta.env.VITE_SECRET_KEY}`;
@@ -32,35 +42,70 @@ function WeatherApp() {
             if (res.ok) {
                 const response = await res.json();
                 setCity(response)
+                // console.log(response.weather[0].description);
+                setTempText(response.weather[0].description);
+                // console.log(response.weather[0].icon);
 
-                if ((response.main.temp - 273.15) < -20) {
-                    setTempImage(snow_icon)
-                    setTempText("Snowy")
+                switch (response.weather[0].icon) {
+                    case '01d':
+                        setTempImage(`${icon_01d}`)
+                        break;
+                    case '01n':
+                        setTempImage(`${icon_01n}`)
+                        break;
+                    case '02d':
+                        setTempImage(`${icon_02d}`)
+                        break;
+                    case '02n':
+                        setTempImage(`${icon_02n}`)
+                        break;
+                    case '03d':
+                        setTempImage(`${icon_03d}`)
+                        break;
+                    case '03n':
+                        setTempImage(`${icon_03n}`)
+                        break;
+                    case '04d':
+                        setTempImage(`${icon_04d}`)
+                        break;
+                    case '04n':
+                        setTempImage(`${icon_04n}`)
+                        break;
+                    case '09d':
+                        setTempImage(`${icon_09d}`)
+                        break;
+                    case '09n':
+                        setTempImage(`${icon_09n}`)
+                        break;
+                    case '10d':
+                        setTempImage(`${icon_10d}`)
+                        break;
+                    case '10n':
+                        setTempImage(`${icon_10n}`)
+                        break;
+                    case '11d':
+                        setTempImage(`${icon_11d}`)
+                        break;
+                    case '11n':
+                        setTempImage(`${icon_11n}`)
+                        break;
+                    case '13d':
+                        setTempImage(`${icon_13d}`)
+                        break;
+                    case '13n':
+                        setTempImage(`${icon_13n}`)
+                        break;
+                    case '50d':
+                        setTempImage(`${icon_50d}`)
+                        break;
+                    case '50n':
+                        setTempImage(`${icon_50n}`)
+                        break;
+
+                    default:
+                        break;
                 }
-                else if ((response.main.temp - 273.15) < 0) {
-                    setTempImage(rain_icon)
-                    setTempText("Rainy")
-                }
-                else if ((response.main.temp - 273.15) < 10) {
-                    setTempImage(drizzle_icon)
-                    setTempText("Drizzle")
-                }
-                else if ((response.main.temp - 273.15) < 20) {
-                    setTempImage(cloud_icon)
-                    setTempText("Cloudy")
-                }
-                else if ((response.main.temp - 273.15) < 30) {
-                    setTempImage(clear_icon)
-                    setTempText("Clear")
-                }
-                else if ((response.main.temp - 273.15) < 35) {
-                    setTempImage(warm_icon)
-                    setTempText("Warm")
-                }
-                else if ((response.main.temp - 273.15) > 35) {
-                    setTempImage(extremeWarm_icon)
-                    setTempText("Warm")
-                }
+
                 console.log(search);
             } else {
                 alert('City not found');
@@ -89,12 +134,12 @@ function WeatherApp() {
             </div>
             {
                 !city ? (
-                    <p>Hello</p>
+                    <p> Welcome to the weather app 🙏</p>
                 ) : (
                     <div className='material'>
 
                         <div className="weatherImage vibrate-1">
-                            <img src={tempImage} className='weatherImageIcon' alt="" />
+                            <img src={tempImage} className='weatherImageIcon color-change' alt="" />
                         </div>
 
 
